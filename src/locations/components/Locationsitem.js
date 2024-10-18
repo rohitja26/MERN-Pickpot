c50013ef1f8ef2d18968cdee6d9887ab6b5cc0b8
Huzaif Shaikh <huzaifsk12@gmail.com>
2024-10-18 15:40:46 +0530

    changed the background and added gradient changed navbar background improved user items card and added animation to it changed text color of locations added condition in location image for better experience

--- src/locations/components/Locationsitem.js
+++ src/locations/components/Locationsitem.js
@@ -1,12 +1,15 @@
 import React from "react";
+import NoImage from "./assets/no-image.png";
 
 import "./Locationsitem.css";
 const LocationsItem = (props) => {
+  const imageSrc = props.pic && props.pic.length > 0 ? props.pic : NoImage;
+
   return (
     <li className="locationitem">
       <div className="locationitem-content">
         <div className="locationitem-pic">
-          <img src={`${props.pic}`} alt={props.title} />
+          <img src={`${imageSrc}`} alt={props.title} />
         </div>
         <div className="locationitem-infor">
           <h2>{props.title}</h2>
--- src/users/components/Usersitem.js
+++ src/users/components/Usersitem.js
@@ -7,15 +7,16 @@ const UsersItem = (props) => {
     <li className="useritem">
       <div className="useritem-content">
         <Link to={`${props.id}/locations`}>
-        <div className="useritem-pic">
-          <img src = {props.pic} width="220px" alt={props.name}></img>
-        </div>
-        <div className="useritem-infor">
-          <h2>{props.name}</h2>
-          <h3>
-            {props.locationcount} {props.locationcount === 1 ? "location" : "locations"}
-          </h3>
-        </div>
+          <div className="useritem-pic">
+            <img src={props.pic} width="220px" alt={props.name}></img>
+          </div>
+          <div className="useritem-infor">
+            <h2>{props.name}</h2>
+            <h3>
+              {props.locationcount}{" "}
+              {props.locationcount === 1 ? "location" : "locations"}
+            </h3>
+          </div>
         </Link>
       </div>
     </li>
